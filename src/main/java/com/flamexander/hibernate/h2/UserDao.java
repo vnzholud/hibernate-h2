1427c71e708e89b6a80a67b36dfd980aa51e3674
vnzholud <vnzholud@mail.ru>
2022-09-05 15:03:44 +0600

    работа с хибернейт

---
+++ src/main/java/com/flamexander/hibernate/h2/UserDao.java
@@ -0,0 +1,15 @@
+package com.flamexander.hibernate.h2;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    User findById(Long id);
+    User findByName(String name);
+    List<User> findAll();
+    void save(User user);
+    void updateNameById(Long id, String newName);
+    void testCaching();
+}
+
+
